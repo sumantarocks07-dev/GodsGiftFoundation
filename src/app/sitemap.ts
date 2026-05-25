887448cea834b591af2c0933fb15c953f3aea652
Sumanta Das <sumantarocks07@gmail.com>
2026-05-26 00:18:05 +0530

    f

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://godsgiftfoundation.org";
+  const baseUrl = "https://godsgiftfoundation.in";
 
   return [
     {
